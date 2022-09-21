fe11828dfb591e43f593eab86f982f962f745f77
flare-s <webflarez@gmail.com>
2022-09-21 11:59:35 +0100

    Added a form > > Co-authored-by: abby <abbychien@hotmail.com> Co-authored-by: Abdullah <webflarez@gmail.com>

--- src/server.js
+++ src/server.js
@@ -5,7 +5,24 @@ const bodyParser = express.urlencoded();
 const server = express();
 
 server.get("/", (req, res) => {
-    res.send(`<h1>Hello!</h1>`);
-  });
+  const content = `<!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        </head>
+        <body>
+        <h1>Micro Blog</h1>
+        <form method="POST" action="/">
+            <label for="name">Insert your name</label>
+            <input type="text" name="name" id="name"/>
+            <label for="message">Write your post</label>
+            <textarea name="message" id="message"></textarea>
+            <button type="submit">Post</button>
+        </form>
+        </body>
+    </html>`;
+  res.send(content);
+});
 
 module.exports = server;
